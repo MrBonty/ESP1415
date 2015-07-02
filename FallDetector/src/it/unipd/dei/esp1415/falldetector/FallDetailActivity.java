ec1dd55045a1617a49b31dfd30e97041a95ac9d7
Asus <Asus@Asus-PC>
2015-07-02 11:55:29 +0200

    Change Fragment to Activity

--- FallDetector/src/it/unipd/dei/esp1415/falldetector/FallDetailActivity.java
+++ FallDetector/src/it/unipd/dei/esp1415/falldetector/FallDetailActivity.java
@@ -1,17 +1,28 @@
 package it.unipd.dei.esp1415.falldetector;
 
-import it.unipd.dei.esp1415.falldetector.fragment.FallSessionFragment;
+import java.util.ArrayList;
+
+import it.unipd.dei.esp1415.falldetector.utility.Fall;
+import it.unipd.dei.esp1415.falldetector.utility.Mediator;
+import it.unipd.dei.esp1415.falldetector.utility.Session;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class FallDetailActivity extends ActionBarActivity {
 	
-	private static int nCurrentFallPosition;
+	private static ArrayList<Session> mArray;
+	private static int mIndex;
+	private Mediator mMod;
+	private static ViewHolder viewHolder;
+	private Fall current;
+    private static int mCurrentFall; //the position of the current Fall
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +30,50 @@ public class FallDetailActivity extends ActionBarActivity {
 
 		setContentView(R.layout.activity_fall);
 		
-		nCurrentFallPosition = getIntent().getExtras().getInt("nFall");
+		mCurrentFall = getIntent().getExtras().getInt("nFall");
 		
-		Fragment FallSession = new FallSessionFragment(nCurrentFallPosition);
-		Log.i("Fall Session", "Activity");
+    	Log.i("Fall Session", "Activity");
 		
-		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-		transaction.replace(R.id.fall, FallSession);
+		mMod = new Mediator();
+		mArray = mMod.getDataSession();
 
-		transaction.commit();
+		viewHolder= new ViewHolder();
+		
+		viewHolder.lt = (LinearLayout) findViewById(R.id.fall_layout);
+		viewHolder.sessionImage = (ImageView) findViewById(R.id.fall_thumbnail);
+		viewHolder.sessionDate = (TextView) findViewById(R.id.fall_date);
+		viewHolder.sessionTime = (TextView) findViewById(R.id.fall_time);
+		viewHolder.sessionLatitude = (TextView) findViewById(R.id.latitude);
+		viewHolder.sessionLongitude = (TextView) findViewById(R.id.longitude);
+		viewHolder.sessionSended = (TextView) findViewById(R.id.sended);
+		
+		viewHolder.lt.setVisibility(View.VISIBLE);
+		
+		mIndex = mMod.getCurretnPosSession();
+		
+		Log.i("Fall Session", "Activity "+mIndex);
+		Log.i("Fall Session", "Activity "+mCurrentFall);
+		
+		current = mArray.get(mIndex).getFall(mCurrentFall); //get the current Fall
+		
+		String[] tmp = current.dateTimeStampFallEven();
+
+		viewHolder.sessionImage.setImageBitmap(mArray.get(mIndex).getBitmap());
+		viewHolder.sessionDate.setText("Happen on "+tmp[0]);
+		viewHolder.sessionTime.setText("at "+tmp[1]);
+		viewHolder.sessionLatitude.setText("Latitude: "); //TODO INSERT METHOD TO GET THE LATITUDE
+		viewHolder.sessionLongitude.setText("Longitude:"); //TODO INSERT METHOD TO GET THE LONGITUDE
+		
+		Log.i("Fall Session", "Activity "+tmp[0]);
+		Log.i("Fall Session", "Activity "+tmp[1]);
+		
+		boolean send = current.isNotified();
+		 
+		String s;
+		if(send) s = "Sended";
+		else s = "Not sended";
+		
+		viewHolder.sessionSended.setText(s);
 	}
 
 	@Override
@@ -47,5 +93,15 @@ public class FallDetailActivity extends ActionBarActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	private class ViewHolder{
+		private LinearLayout lt;
+		private TextView sessionDate;
+		private TextView sessionTime;
+		private TextView sessionLongitude;
+		private ImageView sessionImage;
+		private TextView sessionLatitude;
+		private TextView sessionSended;
 	}
 }
