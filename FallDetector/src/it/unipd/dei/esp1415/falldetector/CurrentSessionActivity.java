8fcc5e75877bca162b3690a517bad5ffe8387aa6
MrBonty <bontempo.omar@gmail.com>
2015-08-25 14:25:45 +0200

    fix

--- FallDetector/src/it/unipd/dei/esp1415/falldetector/CurrentSessionActivity.java
+++ FallDetector/src/it/unipd/dei/esp1415/falldetector/CurrentSessionActivity.java
@@ -554,7 +554,6 @@ public class CurrentSessionActivity extends ActionBarActivity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
